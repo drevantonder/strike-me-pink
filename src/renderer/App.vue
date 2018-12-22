00334d12c70ee3862ef0899869e6837e01bf8abd
Andre van Tonder <drevtonder@gmail.com>
2018-12-22 15:23:27 +0800

    👶 The humble beginning of the sound desk

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'sound-desk'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
